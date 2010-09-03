07bbab43256780d8b1c582ece1099c9010677bd4
Joe Riedel <joeriedel@hotmail.com>
2010-09-03 04:42:43 -0500

    IOS Work

---
+++ Source/XCode/IOS/MakeMeBuild.cpp
@@ -0,0 +1,13 @@
+/*
+ *  MakeMeBuild.cpp
+ *  Balance
+ *
+ *  Created by Joseph Riedel on 9/3/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+int make_me_build()
+{
+	return 0;
+}
