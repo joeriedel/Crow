39b05752a299cdfe3146134181d638e92113a6b3
Joe Riedel <joeriedel@hotmail.com>
2011-05-02 15:01:48 -0500

    Missed files.
    Signed-off-by: Joe Riedel <joeriedel@hotmail.com>

---
+++ Source/Game/Entities/G_ScreenProjectile.cpp
@@ -0,0 +1,122 @@
+// G_ScreenProjectile.cpp
+// Copyright (c) 2010 Pyramind Labs LLC, All Rights Reserved
+// Author: Joe Riedel (joeriedel@hotmail.com)
+// See Radiance/LICENSE for licensing terms.
+
+#include "G_ScreenProjectile.h"
+#include <Engine/World/WorldLua.h>
+
+namespace world {
+
+G_ScreenProjectile::G_ScreenProjectile() : 
+E_CONSTRUCT_BASE
+{
+	m_distance[0] = m_distance[1] = 0.f;
+}
+
+G_ScreenProjectile::~G_ScreenProjectile()
+{
+}
+
+void G_ScreenProjectile::PushCallTable(lua_State *L)
+{
+	Entity::PushCallTable(L);
+	lua_pushcfunction(L, lua_Fire);
+	lua_setfield(L, -2, "NativeFire");
+}
+
+void G_ScreenProjectile::DoSetLuaState(lua_State *L)
+{
+	Entity::DoSetLuaState(L);
+
+	lua_createtable(L, 2, 0);
+	lua_pushinteger(L, 1);
+	lua_pushnumber(L, m_distance[0]);
+	lua_settable(L, -3);
+	lua_pushinteger(L, 2);
+	lua_pushnumber(L, m_distance[1]);
+	lua_settable(L, -3);
+	lua_setfield(L, -2, "distance");
+}
+
+void G_ScreenProjectile::Fire(const Vec3 &_start, const Vec3 &_end)
+{
+	Entity::Ref parent = m_ps.parent.lock();
+	if (!parent)
+		return;
+
+	m_distance[0] = 0.f;
+	m_distance[1] = (_end-_start).Magnitude();
+	
+	Mat4 m = Mat4::Rotation(QuatFromAngles(Vec3(0, 0, parent->ps->worldAngles[2])));
+	Vec3 fwd = Vec3(1, 0, 0) * m;
+	Vec3 up, left;
+	FrameVecs(fwd, up, left);
+
+	Vec3 start = _start-parent->ps->worldPos;
+	Vec3 end   = _end-parent->ps->worldPos;
+
+	m_start[0] = left.Dot(start);
+	m_start[1] = up.Dot(start);
+	m_start[2] = fwd.Dot(start);
+
+	m_end[0] = left.Dot(end);
+	m_end[1] = up.Dot(end);
+	m_end[2] = fwd.Dot(end);
+}
+
+void G_ScreenProjectile::TickPhysics(
+	int frame, 
+	float dt, 
+	const xtime::TimeSlice &time
+)
+{
+	Entity::Ref parent = m_ps.parent.lock();
+	if (!parent)
+		return;
+
+	TickOther(*parent, frame, dt, time);
+
+	m_distance[0] += m_ps.velocity.Magnitude()*dt;
+
+	Mat4 m = Mat4::Rotation(QuatFromAngles(Vec3(0, 0, parent->ps->worldAngles[2])));
+	Vec3 fwd = Vec3(1, 0, 0) * m;
+	Vec3 up, left;
+	FrameVecs(fwd, up, left);
+
+	Vec3 pos = math::Lerp(m_start, m_end, m_distance[0]/m_distance[1]);
+
+	Vec3 start = parent->ps->worldPos+(left*m_start[0])+(up*m_start[1])+(fwd*m_start[2]);
+	Vec3 end   = parent->ps->worldPos+(left*m_end[0])+(up*m_end[1])+(fwd*m_end[2]);
+	
+	m_ps.pos = parent->ps->worldPos+(left*pos[0])+(up*pos[1])+(fwd*pos[2]);
+	m_ps.cameraPos = m_ps.pos;
+	m_ps.angles = LookAngles((end-start).Normalize());
+	m_ps.worldAngles = WrapAngles(m_ps.originAngles + m_ps.angles);
+	Move(true, false);
+}
+
+int G_ScreenProjectile::lua_Fire(lua_State *L)
+{
+	G_ScreenProjectile *self = static_cast<G_ScreenProjectile*>(WorldLua::EntFramePtr(L, 1, true));
+	Vec3 start = lua::Marshal<Vec3>::Get(L, 2, true);
+	Vec3 end = lua::Marshal<Vec3>::Get(L, 3, true);
+
+	self->SyncLuaState(L); // make sure to pull in m_ps.parent
+	self->Fire(start, end);
+	self->SetLuaState(L);
+
+	return 0;
+}
+
+} // world
+
+
+namespace spawn {
+
+void *info_projectile::Create()
+{
+	return new (ZWorld) world::G_ScreenProjectile();
+}
+
+} // spawn
---
+++ Source/Game/Entities/G_ScreenProjectile.h
@@ -0,0 +1,53 @@
+// G_ScreenProjectile.h
+// Copyright (c) 2010 Pyramind Labs LLC, All Rights Reserved
+// Author: Joe Riedel (joeriedel@hotmail.com)
+// See Radiance/LICENSE for licensing terms.
+
+#pragma once
+
+#include <Engine/World/Entity.h>
+#include <Runtime/PushPack.h>
+
+namespace world {
+
+class G_ScreenProjectile : public Entity
+{
+	E_DECL_BASE(Entity);
+public:
+
+	G_ScreenProjectile();
+	virtual ~G_ScreenProjectile();
+
+	void Fire(const Vec3 &start, const Vec3 &end);
+
+protected:
+	
+	virtual void PushCallTable(lua_State *L);
+	virtual void DoSetLuaState(lua_State *L);
+
+	virtual void TickPhysics(
+		int frame, 
+		float dt, 
+		const xtime::TimeSlice &time
+	);
+
+private:
+
+	RAD_DECLARE_GET(start, const Vec3&) { return m_start; }
+	RAD_DECLARE_SET(start, const Vec3&) { m_start = value; }
+	RAD_DECLARE_GET(end, const Vec3&) { return m_end; }
+	RAD_DECLARE_SET(end, const Vec3&) { m_end = value; }
+
+	static int lua_Fire(lua_State *L);
+
+	Vec3 m_start;
+	Vec3 m_end;
+	float m_distance[2];
+	
+};
+
+} // world
+
+E_DECL_SPAWN(RADNULL_API, info_projectile)
+
+#include <Runtime/PopPack.h>
