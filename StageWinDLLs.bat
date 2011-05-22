fb70b8c6c00025fcb8e5f3254625a1f77bb99eeb
Joe Riedel <joeriedel@hotmail.com>
2011-05-22 03:19:05 -0500

    Sound / Music system. No game hooks yet.
    Signed-off-by: Joe Riedel <joeriedel@hotmail.com>

--- StageWinDLLs.bat
+++ StageWinDLLs.bat
@@ -8,10 +8,8 @@ cd ..
 
 xcopy Radiance\Extern\Win\Cg\2.2\bin\cg.dll Bin\win-msvc8-debug /D
 xcopy Radiance\Extern\Win\Cg\2.2\bin\cgGL.dll Bin\win-msvc8-debug /D
-xcopy Radiance\Extern\Win\Cg\2.2\bin\cg.dll Bin\win-msvc8-debug /D
 xcopy Radiance\Extern\Win\Cg\2.2\bin\cg.dll Bin\win-msvc8-developer /D
 xcopy Radiance\Extern\Win\Cg\2.2\bin\cgGL.dll Bin\win-msvc8-developer /D
-xcopy Radiance\Extern\Win\Cg\2.2\bin\cg.dll Bin\win-msvc8-developer /D
 
 xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtCored4.dll Bin\win-msvc8-debug /D
 xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtGuid4.dll Bin\win-msvc8-debug /D
@@ -27,3 +25,7 @@ xcopy Radiance\Extern\Win\SDL\1.2\SDL.dll Bin\win-msvc8-golden /D
 xcopy Radiance\Extern\Win\VLD\1.9h\VS8\bin\dbghelp.dll Bin\win-msvc8-debug /D
 xcopy Radiance\Extern\Win\VLD\1.9h\VS8\bin\Microsoft.DTfW.DHL.manifest Bin\win-msvc8-debug /D
 xcopy Radiance\Extern\Win\VLD\1.9h\VS8\bin\vld.dll Bin\win-msvc8-debug /D
+
+xcopy Radiance\Extern\Win\OpenAL\bin\OpenAL32.dll Bin\win-msvc8-debug /D
+xcopy Radiance\Extern\Win\OpenAL\bin\OpenAL32.dll Bin\win-msvc8-developer /D
+xcopy Radiance\Extern\Win\OpenAL\bin\OpenAL32.dll Bin\win-msvc8-golden /D
