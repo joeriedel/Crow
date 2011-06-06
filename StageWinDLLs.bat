cb894ced41aa6b5a2385d7fc8d282d8b08504143
Joe Riedel <joeriedel@hotmail.com>
2011-06-06 00:34:30 -0500

    Texture compression.
    Signed-off-by: Joe Riedel <joeriedel@hotmail.com>

--- StageWinDLLs.bat
+++ StageWinDLLs.bat
@@ -10,6 +10,8 @@ xcopy Radiance\Extern\Win\Cg\2.2\bin\cg.dll Bin\win-msvc8-debug /D
 xcopy Radiance\Extern\Win\Cg\2.2\bin\cgGL.dll Bin\win-msvc8-debug /D
 xcopy Radiance\Extern\Win\Cg\2.2\bin\cg.dll Bin\win-msvc8-developer /D
 xcopy Radiance\Extern\Win\Cg\2.2\bin\cgGL.dll Bin\win-msvc8-developer /D
+xcopy Radiance\Extern\Win\Cg\2.2\bin\cg.dll Bin\win-msvc8-developer-sym /D
+xcopy Radiance\Extern\Win\Cg\2.2\bin\cgGL.dll Bin\win-msvc8-developer-sym /D
 
 xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtCored4.dll Bin\win-msvc8-debug /D
 xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtGuid4.dll Bin\win-msvc8-debug /D
@@ -17,9 +19,13 @@ xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtOpenGLd4.dll Bin\win-msvc8-debu
 xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtCore4.dll Bin\win-msvc8-developer /D
 xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtGui4.dll Bin\win-msvc8-developer /D
 xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtOpenGL4.dll Bin\win-msvc8-developer /D
+xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtCore4.dll Bin\win-msvc8-developer-sym /D
+xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtGui4.dll Bin\win-msvc8-developer-sym /D
+xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtOpenGL4.dll Bin\win-msvc8-developer-sym /D
 
 xcopy Radiance\Extern\Win\SDL\1.2\SDL.dll Bin\win-msvc8-debug /D
 xcopy Radiance\Extern\Win\SDL\1.2\SDL.dll Bin\win-msvc8-developer /D
+xcopy Radiance\Extern\Win\SDL\1.2\SDL.dll Bin\win-msvc8-developer-sym /D
 xcopy Radiance\Extern\Win\SDL\1.2\SDL.dll Bin\win-msvc8-golden /D
 
 xcopy Radiance\Extern\Win\VLD\1.9h\VS8\bin\dbghelp.dll Bin\win-msvc8-debug /D
@@ -28,4 +34,9 @@ xcopy Radiance\Extern\Win\VLD\1.9h\VS8\bin\vld.dll Bin\win-msvc8-debug /D
 
 xcopy Radiance\Extern\Win\OpenAL\bin\OpenAL32.dll Bin\win-msvc8-debug /D
 xcopy Radiance\Extern\Win\OpenAL\bin\OpenAL32.dll Bin\win-msvc8-developer /D
+xcopy Radiance\Extern\Win\OpenAL\bin\OpenAL32.dll Bin\win-msvc8-developer-sym /D
 xcopy Radiance\Extern\Win\OpenAL\bin\OpenAL32.dll Bin\win-msvc8-golden /D
+
+xcopy Radiance\Extern\Win\PVRTexLib\x32\PVRTexLib.dll Bin\win-msvc8-debug /D
+xcopy Radiance\Extern\Win\PVRTexLib\x32\PVRTexLib.dll Bin\win-msvc8-developer /D
+xcopy Radiance\Extern\Win\PVRTexLib\x32\PVRTexLib.dll Bin\win-msvc8-developer-sym /D
