8e006918bf47693c58d49483619898340c184416
zoid <joeriedel@hotmail.com>
2011-09-18 01:33:47 -0500

    Updated stagedlls.
    Signed-off-by: zoid <joeriedel@hotmail.com>

--- StageWinDLLs.bat
+++ StageWinDLLs.bat
@@ -7,37 +7,37 @@ mkdir win-msvc8-golden
 cd ..
 
 
-xcopy Radiance\Extern\Win\Cg\2.2\bin\cg.dll Bin\win-msvc8-debug /D
-xcopy Radiance\Extern\Win\Cg\2.2\bin\cgGL.dll Bin\win-msvc8-debug /D
-xcopy Radiance\Extern\Win\Cg\2.2\bin\cg.dll Bin\win-msvc8-developer /D
-xcopy Radiance\Extern\Win\Cg\2.2\bin\cgGL.dll Bin\win-msvc8-developer /D
-xcopy Radiance\Extern\Win\Cg\2.2\bin\cg.dll Bin\win-msvc8-developer-sym /D
-xcopy Radiance\Extern\Win\Cg\2.2\bin\cgGL.dll Bin\win-msvc8-developer-sym /D
+xcopy Radiance\Extern\Win\Cg\2.2\bin\cg.dll Bin\win-msvc8-debug /D /Y
+xcopy Radiance\Extern\Win\Cg\2.2\bin\cgGL.dll Bin\win-msvc8-debug /D /Y
+xcopy Radiance\Extern\Win\Cg\2.2\bin\cg.dll Bin\win-msvc8-developer /D /Y
+xcopy Radiance\Extern\Win\Cg\2.2\bin\cgGL.dll Bin\win-msvc8-developer /D /Y
+xcopy Radiance\Extern\Win\Cg\2.2\bin\cg.dll Bin\win-msvc8-developer-sym /D /Y
+xcopy Radiance\Extern\Win\Cg\2.2\bin\cgGL.dll Bin\win-msvc8-developer-sym /D /Y
 
-xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtCored4.dll Bin\win-msvc8-debug /D
-xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtGuid4.dll Bin\win-msvc8-debug /D
-xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtOpenGLd4.dll Bin\win-msvc8-debug /D
-xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtCore4.dll Bin\win-msvc8-developer /D
-xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtGui4.dll Bin\win-msvc8-developer /D
-xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtOpenGL4.dll Bin\win-msvc8-developer /D
-xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtCore4.dll Bin\win-msvc8-developer-sym /D
-xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtGui4.dll Bin\win-msvc8-developer-sym /D
-xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtOpenGL4.dll Bin\win-msvc8-developer-sym /D
+xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtCored4.dll Bin\win-msvc8-debug /D /Y
+xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtGuid4.dll Bin\win-msvc8-debug /D /Y
+xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtOpenGLd4.dll Bin\win-msvc8-debug /D /Y
+xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtCore4.dll Bin\win-msvc8-developer /D /Y
+xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtGui4.dll Bin\win-msvc8-developer /D /Y
+xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtOpenGL4.dll Bin\win-msvc8-developer /D /Y
+xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtCore4.dll Bin\win-msvc8-developer-sym /D /Y
+xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtGui4.dll Bin\win-msvc8-developer-sym /D /Y
+xcopy Radiance\Extern\Win\Qt\4.5.2\vs8-dll\lib\QtOpenGL4.dll Bin\win-msvc8-developer-sym /D /Y
 
-xcopy Radiance\Extern\Win\SDL\1.2\SDL.dll Bin\win-msvc8-debug /D
-xcopy Radiance\Extern\Win\SDL\1.2\SDL.dll Bin\win-msvc8-developer /D
-xcopy Radiance\Extern\Win\SDL\1.2\SDL.dll Bin\win-msvc8-developer-sym /D
-xcopy Radiance\Extern\Win\SDL\1.2\SDL.dll Bin\win-msvc8-golden /D
+xcopy Radiance\Extern\Win\SDL\1.2\SDL.dll Bin\win-msvc8-debug /D /Y
+xcopy Radiance\Extern\Win\SDL\1.2\SDL.dll Bin\win-msvc8-developer /D /Y
+xcopy Radiance\Extern\Win\SDL\1.2\SDL.dll Bin\win-msvc8-developer-sym /D /Y
+xcopy Radiance\Extern\Win\SDL\1.2\SDL.dll Bin\win-msvc8-golden /D /Y
 
-xcopy Radiance\Extern\Win\VLD\1.9h\VS8\bin\dbghelp.dll Bin\win-msvc8-debug /D
-xcopy Radiance\Extern\Win\VLD\1.9h\VS8\bin\Microsoft.DTfW.DHL.manifest Bin\win-msvc8-debug /D
-xcopy Radiance\Extern\Win\VLD\1.9h\VS8\bin\vld.dll Bin\win-msvc8-debug /D
+xcopy Radiance\Extern\Win\VLD\1.9h\VS8\bin\dbghelp.dll Bin\win-msvc8-debug /D /Y
+xcopy Radiance\Extern\Win\VLD\1.9h\VS8\bin\Microsoft.DTfW.DHL.manifest Bin\win-msvc8-debug /D /Y
+xcopy Radiance\Extern\Win\VLD\1.9h\VS8\bin\vld.dll Bin\win-msvc8-debug /D /Y
 
-xcopy Radiance\Extern\Win\OpenAL\bin\OpenAL32.dll Bin\win-msvc8-debug /D
-xcopy Radiance\Extern\Win\OpenAL\bin\OpenAL32.dll Bin\win-msvc8-developer /D
-xcopy Radiance\Extern\Win\OpenAL\bin\OpenAL32.dll Bin\win-msvc8-developer-sym /D
-xcopy Radiance\Extern\Win\OpenAL\bin\OpenAL32.dll Bin\win-msvc8-golden /D
+xcopy Radiance\Extern\Win\OpenAL\bin\softoal\OpenAL32.dll Bin\win-msvc8-debug /D /Y
+xcopy Radiance\Extern\Win\OpenAL\bin\softoal\OpenAL32.dll Bin\win-msvc8-developer /D /Y
+xcopy Radiance\Extern\Win\OpenAL\bin\softoal\OpenAL32.dll Bin\win-msvc8-developer-sym /D /Y
+xcopy Radiance\Extern\Win\OpenAL\bin\softoal\OpenAL32.dll Bin\win-msvc8-golden /D /Y
 
-xcopy Radiance\Extern\Win\PVRTexLib\x32\PVRTexLib.dll Bin\win-msvc8-debug /D
-xcopy Radiance\Extern\Win\PVRTexLib\x32\PVRTexLib.dll Bin\win-msvc8-developer /D
-xcopy Radiance\Extern\Win\PVRTexLib\x32\PVRTexLib.dll Bin\win-msvc8-developer-sym /D
+xcopy Radiance\Extern\Win\PVRTexLib\x32\PVRTexLib.dll Bin\win-msvc8-debug /D /Y
+xcopy Radiance\Extern\Win\PVRTexLib\x32\PVRTexLib.dll Bin\win-msvc8-developer /D /Y
+xcopy Radiance\Extern\Win\PVRTexLib\x32\PVRTexLib.dll Bin\win-msvc8-developer-sym /D /Y
