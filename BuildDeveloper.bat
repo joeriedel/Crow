c099061f8913dffdfe8f940425b6acc61bc57ca4
joer <joer@sunsidegames.com>
2012-07-17 04:14:23 +0300

    Fixing BuildDeveloper.bat file.
    Signed-off-by: joer <joer@sunsidegames.com>

--- BuildDeveloper.bat
+++ BuildDeveloper.bat
@@ -1,4 +1,5 @@
-call "%VS100COMNTOOLS%\..\..\VC\vcvarsall.bat" x86
-devenv Source/VSProjects/VS10/Crow.sln /rebuild "Dev - Release|Win32"
-
+call "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat" x86
+devenv Source/VSProjects/VS10/Crow.sln /clean "Dev - Release|Win32"
+devenv Source/VSProjects/VS10/Crow.sln /build "Dev - Release|Win32" /project QGames
+devenv Source/VSProjects/VS10/Crow.sln /build "Dev - Release|Win32" /project Crow
 pause
